e96505dcd2524c36e3460f31a2acfd9974700d53
wzw5566 <wzw33874@qq.com>
2018-08-19 23:13:53 +0800

    项目创建

---
+++ MxShop/__init__.py
@@ -0,0 +1,6 @@
+import pymysql
+
+#设置mysql数据库驱动
+pymysql.install_as_MySQLdb()
+
+
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/vincentwen/MyCode/Django/MxShop/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
